edd9002effc758fe718f8d4ed45d282c0b58e878
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-22 12:52:30 +0000

    Automatically backed up by Learn

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,10 @@
+class IngredientsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 FlatironKitchen::Application.routes.draw do
+  get 'ingredients/new'
+
+  get 'ingredients/create'
+
+  get 'ingredients/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
