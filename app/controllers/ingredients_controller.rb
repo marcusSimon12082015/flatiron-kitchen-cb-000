b614ddc140df11ce9196c3f2c71231c1e98916f9
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-22 13:15:27 +0000

    Automatically backed up by Learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,10 +1,15 @@
 class IngredientsController < ApplicationController
   def new
     @ingredient = Ingredient.new
-
   end
 
   def create
+    @ingredient = Ingredient.new(params[:name])
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      redirect_to "new"
+    end
   end
 
   def edit
